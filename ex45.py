cc70d9bc8c905345e46b778d1fb6e75ca946cec9
wzpdreamer <3176869767@qq.com>
2019-05-07 19:21:16 +0800

    继承与组合

--- ex45.py
+++ ex45.py
@@ -23,3 +23,27 @@ son.override()
 dad.altered()
 son.altered()
 
+class Other():
+    def override(self):
+        print("Other override()")
+    def implicit(self):
+        print("Other implicit()")
+    def altered(self):
+        print("Other altered()")
+
+class Child():
+    def __init__(self):
+        self.other = Other()
+    def implicit(self):
+        self.other.implicit()
+    def override(self):
+        print("Child override()")
+    def altered(self):
+        print("Child, before other altered()")
+        self.other.altered()
+        print("Child, after other altered()")
+
+son = Child()
+son.implicit()
+son.override()
+son.altered()
